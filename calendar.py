89250072fed633120b263367c0ce73140140dcbb
Iain Knox <iain.knox1@btinternet.com>
2022-07-14 07:13:03 +0000

    create functionality for calendar. add structure to view and update

--- calendar.py
+++ calendar.py
@@ -8,19 +8,55 @@ simple prototype for my P4 project.
 from time import sleep, strftime
 
 USER = 'name'
-calendar = {}
+calendar = {}   # create an empty dict to hold day:event pairs
 
 
 def welcome():
     '''
-    function to get user details and start
+    function to welcomew a user, get user details and start
     the calendar.
     '''
     print(f'Welcome to the Calendar {USER}')
     print('The calendar is starting up...')
+    # let the user know the app is starting
     sleep(1)
     print('Today is ' + strftime('%A %m %d %Y'))
+    # print Full weekday name, Month, Day, Year
     print('The current time is: ' + strftime('%H:%M:%S'))
+    # print Hour:Minutes:Seconds
     sleep(1)
     print('What would you like to do? ')
 
+
+def start_calendar():
+    '''
+    this function holds all the calendar functionality
+    to view, add, update, delete and exit the app.
+    '''
+    welcome()   # here I call welcome, to welcome the user.
+    start = True
+    # the User can decide when to exit the app, so the
+    # start variable holds a boolean to test when the
+    # user opts to close the app.
+    while start:
+        # create the functionality a user can choose from
+        user_choice = input('Press A to add, \
+        U to Update, \
+        V to View, \
+        D to Delete or \
+        X to exit: ')
+        user_choice = user_choice.upper()   # change to uppercase
+        if user_choice == 'V':  # view calendar
+            if len(calendar.keys()) < 1:
+                # check if there are existing entries by checking
+                # that the length of the array is greater than 1
+                print('Your calendar is empty')
+            else:
+                print(calendar)
+        elif user_choice == 'U':    # update calendar
+            date = input('Which date would you like to update? ')
+            update = input('Enter the update: ')
+            # allow the user to enter a string as an update
+            calendar[date] = update  # adds the update to the specified date
+            print('Calendar updated successfully.')
+            print(calendar)  # to display the update
